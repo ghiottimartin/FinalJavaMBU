68d09695097f0476a74178af72444ed0d6d95141
martinghi15 <martinghiotti@yahoo.com.ar>
2019-08-28 12:25:14 -0700

    Validacion de creacion de usuario, de email y contraseña

--- src/servlets/Registro.java
+++ src/servlets/Registro.java
@@ -8,88 +8,114 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.catalina.connector.Request;
+
 import logic.CtrlRegistro;
 import entidades.Usuario;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 /**
  * Servlet implementation class Registro
  */
 @WebServlet("/Registro")
 public class Registro extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Registro() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Registro() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
-		if(request.getParameter("registrar")!= null){
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		if (request.getParameter("registrar") != null) {
 			CtrlRegistro ctrl = new CtrlRegistro();
 			Usuario currentUser = this.mapUserFromForm(request);
 			try {
-				//hacer validaciones
-				//if(this.validateUser(currentUser, request.getParameter("passwordRepeated"))){
+				// hacer validaciones
+				// if(this.validateUser(currentUser, request.getParameter("passwordRepeated"))){
+				request.getSession().setAttribute("emailErroneo", "false");
+				if(this.validateMail(currentUser.getEmail())) {
+					request.getSession().setAttribute("emailErroneo", "true");
+				}
+				request.getSession().setAttribute("contraseñaNoCoincide", "false");
+				if(this.validaContraseña(currentUser, request.getParameter("passwordRepeated"))) {
+					request.getSession().setAttribute("contraseñaNoCoincide", "true");
+				}
+				if(!this.validateMail(currentUser.getEmail()) && !this.validaContraseña(currentUser, request.getParameter("passwordRepeated"))) {
 					ctrl.register(currentUser);
 					response.sendRedirect("index.jsp");
-				/*} else {
-			      Cookie c = new Cookie("userName", request.getParameter("nombreUsuario"));
-			      response.addCookie(c);
-			      response.sendRedirect("routes/Registro.jsp");
-			      
-				}*/
+				} else {
+					response.sendRedirect("routes/Registro.jsp");
+				}
+
+				/*
+				 * } else { Cookie c = new Cookie("userName",
+				 * request.getParameter("nombreUsuario")); response.addCookie(c);
+				 * response.sendRedirect("routes/Registro.jsp");
+				 * 
+				 * }
+				 */
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		
-		if(request.getParameter("volver")!= null){
+
+		if (request.getParameter("volver") != null) {
 			response.sendRedirect("index.jsp");
 		}
-					
+
 	}
-	
+
 	public Usuario mapUserFromForm(HttpServletRequest request) {
 		Usuario user = new Usuario();
-		
+
 		user.setNombreUsuario(request.getParameter("nombreUsuario"));
 		user.setPassword(request.getParameter("password"));
 		user.setEmail(request.getParameter("email"));
 		user.setNombre(request.getParameter("nombre"));
 		user.setApellido(request.getParameter("apellido"));
-		
+
 		return user;
 	}
+
+	public boolean validateMail(String email) {
+		Pattern pattern = Pattern.compile(
+				"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+		Matcher mather = pattern.matcher(email);
+ 		if (mather.find() == false) {
+			return true;
+		} else {
+			return false;
+		}
+ 	}
 	
-	public boolean validateUser(Usuario user, String repeatedPassword) {
-		Pattern pattern = Pattern
-                .compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
-        String email = user.getEmail();
-        Matcher mather = pattern.matcher(email);
-        if (mather.find() == false) {
-            return false;
-        }
-        if(user.getPassword() != repeatedPassword){
-        	return false;
-        }
-		return true;
+	public boolean validaContraseña(Usuario user,String contraseña) {
+		System.out.println(user.getPassword().compareTo(contraseña));
+		if (user.getPassword().compareTo(contraseña) != 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
